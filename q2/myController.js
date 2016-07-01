9a51ab516b25c48a281c1ef15b7e31170649143e
prabh12072 <prabhjot12072@iiitd.ac.in>
2016-07-01 14:47:54 +0530

    Update myController.js

--- q2/myController.js
+++ q2/myController.js
@@ -2,25 +2,30 @@ var module= angular.module("myapp", []);
 		module.controller("myctrl", function($scope){
 			$scope.name="Prabhjot";
 			$scope.designation="SDE";
+			$scope.fun= function print()
+			{
+				alert("Pop up");
+				
+			}
 
 		});
 		module.directive("ngOut",function(){
 			return{
-				template: '<div  class="name-body"> <p>output with restrict type A is: </p> {{name}} </div>',
+				template: '<div  class="name-body"> <p>output with restrict type A is: </p> {{name}} <button ng-click="fun()"> Click here</button></div>',
 				restrict: 'A',
 				
 			}
 		});
 			module.directive("ngOut1", function(){
 				return{
-					template: '<div  class="desig-body">  <p>output with restrict type E is: </p> {{designation}} </div>',
+					template: '<div  class="desig-body">  <p>output with restrict type E is: </p> {{designation}} <button ng-click="fun()"> Click here</button></div>',
 					restrict: 'E',
 					
 				}
 			});
 				module.directive("ngOut2", function(){
 				return{
-					template: '<div  class="panel-body"> {{name}} </div>',
+					template: '<div  class="panel-body"> {{name}}<button ng-click="fun()"> Click here</button> </div>',
 					restrict: 'M',
 					
 				}
